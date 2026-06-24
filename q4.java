d917a16cb2aaaa72251cea778d1c061346016a5b
Om Dubey <25113cn251@glbitm.ac.in>
2026-06-24 15:26:23 +0530

    First commit

--- q4.java
+++ q4.java
@@ -2,30 +2,26 @@ import java.util.Scanner;
 
 public class q4 {
     public static void main(String[] args) {
-        
-Scanner sc = new Scanner(System.in);
-System.out.print("enter the number  ");
-   int num = sc.nextInt();
-   int orignalnum=num;
-int rev=0;
-int b =0;
-while (num!=0) {
-     b=num%10;
-     rev = (rev*10)+b;
-     num/=10;
-}
-int rev2 =rev;
-if(orignalnum==rev2){
-System.out.println("it is  palindrome");
 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the number  ");
+        int num = sc.nextInt();
+        int orignalnum = num;
+        int rev = 0;
+        int b = 0;
+        while (num != 0) {
+            b = num % 10;
+            rev = (rev * 10) + b;
+            num /= 10;
+        }
+        int rev2 = rev;
+        if (orignalnum == rev2) {
+            System.out.println("it is  palindrome");
 
-  }
-   else{
-    System.out.println("not palindrome");
-}
+        } else {
+            System.out.println("not palindrome");
+        }
 
-
-     sc.close();
+        sc.close();
     }
 }
-

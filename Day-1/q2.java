d857a0a2267dddbed491d9e8aeb852951f831ac2
Om Dubey <25113cn251@glbitm.ac.in>
2026-06-24 15:53:17 +0530

    Add files via upload

---
+++ Day-1/q2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+ System.out.println(" ");
+ 
+ System.out.println("enter number : ");
+
+
+ int a = sc.nextInt();
+ System.out.println(" ");
+
+ for(int i=1;i<=10;i++){
+
+   
+    System.out.println(a*i);
+
+
+
+ }
+
+     sc.close();
+    }
+}
